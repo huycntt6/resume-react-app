abc156d8e6919e0c59e847902d4d02bffd5b3d88
Hoang Van Huy <huynoone@gmail.com>
2020-12-10 16:24:39 +0700

    update

--- src/component/pages/PageContainer.js
+++ src/component/pages/PageContainer.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import PageRouter from '../../router/PageRouter';
 import { Container, Row, Col } from 'reactstrap';
 import hvh from '../../media/image/hvh.png';
 import './PageContainer.scss';
 
-class MainUser extends React.Component{
-    render(){
-        return(
-            <Container className="cv-main page-style">
-                <Row>
-                    <Col lg={4} className="info-content">
-                        <div className="top">
-                            <div className="top-photo">
-                                <img alt="avatar" draggable="false" src={hvh} />
-                            </div>
-                            <div className="top-title">
-                                <h2>Hoang Van Huy</h2>
-                                <h4>Web Developer</h4>
-                            </div>
+const MainUser = () => {
+    const [height, setHeight] = useState(0);
+    useEffect(() => {
+        const height = document.querySelector('.page-style').offsetHeight;
+        setHeight(height);
+    }, []);
+    return(
+        <Container className="cv-main page-style">
+            <Row>
+                <Col lg={4} className="info-content">
+                    <div className="top">
+                        <div className="top-photo">
+                            <img alt="avatar" draggable="false" src={hvh} />
                         </div>
-                        <div className="cv-button">
-                                <a href="/file/CV-HoangVanHuy.docx" download className="btn btn-primary">Download CV</a>
+                        <div className="top-title">
+                            <h2>Hoang Van Huy</h2>
+                            <h4>Web Developer</h4>
                         </div>
-                    </Col>
-                    <Col lg={8} className="content-area scrollbar">
-                        <PageRouter/>
-                    </Col>
-                    
-                </Row>
+                    </div>
+                    <div className="cv-button">
+                            <a href="/file/CV-HoangVanHuy.docx" download className="btn btn-primary">Download CV</a>
+                    </div>
+                </Col>
+                <Col lg={8} className="content-area scrollbar" style={{maxHeight: `${height}px`}}>
+                    <PageRouter/>
+                </Col>
                 
-            </Container>
-        );
-    }
+            </Row>
+            
+        </Container>
+    );
 }
 
 export default MainUser;
