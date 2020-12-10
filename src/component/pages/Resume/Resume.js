9de06748e7212ddbb9785227a5429cd5c9d93cc7
Hoang Van Huy <huynoone@gmail.com>
2020-12-10 16:34:03 +0700

    Update Resume.js

--- src/component/pages/Resume/Resume.js
+++ src/component/pages/Resume/Resume.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Col, Row } from 'reactstrap';
-import naturix from '../../../media/image/naturix.png';
-import ironstyle from '../../../media/image/ironstyle.png';
-import piwoo from '../../../media/image/piwoo.png';
-import { Link } from 'react-router-dom';
+// import naturix from '../../../media/image/naturix.png';
+// import ironstyle from '../../../media/image/ironstyle.png';
+// import piwoo from '../../../media/image/piwoo.png';
+// import { Link } from 'react-router-dom';
 
 const Resume = () => (
     <section className="resume">
